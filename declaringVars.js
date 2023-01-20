2055350a5a5900b978fbbce9f46cc0877a4699c2
ctwarit1 <ctwarit1@gmail.com>
2023-01-20 15:21:56 +0530

    declaringVars

---
+++ declaringVars.js
@@ -0,0 +1,41 @@
+// Declaring variables with different types
+
+// ****************
+// by using var
+
+// var declaration are **globally scoped or func scoped**
+
+// var a = 10
+//         function f(){
+//             console.log(a);
+//         }
+//     f();
+//     console.log(a);
+
+// ****************
+// by using let 
+ 
+// it is a block scoped means it will execute with in the block only
+
+// let a = 10
+//         function f(){
+//             let b =9;
+//             console.log(b);
+//             console.log(a);
+//         }
+        
+//     f();
+    
+// ****************
+// by using const 
+ 
+//  whenever  variable is declared with const it should be initialized.
+// also it remains constant we cant redefine it
+
+// const a = 10;
+//     function f() {
+//         a = 9
+//         console.log(a);
+//     }
+//     f();
+
