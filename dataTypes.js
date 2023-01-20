b84970d7416ef9ea69580334a99778a1dbb5dc4f
ctwarit1 <ctwarit1@gmail.com>
2023-01-20 15:14:37 +0530

    java script data type

---
+++ dataTypes.js
@@ -0,0 +1,54 @@
+// JS have 8 data types
+
+/*
+ --> String
+ --> Number
+ --> BigInt
+ --> Boolean
+ --> undefined
+ --> Null
+ --> Symbol
+ --> Object
+*/
+
+// **********
+
+// string exmple
+// let a= "Hello! "
+// console.log(a)
+
+// **********;
+
+// number exmple
+// let a= 1;
+// console.log(a);
+
+// **********;
+
+// boolean exmple
+// let a= true;
+// console.log(a);
+
+// **********;
+
+// object exmple
+// let a= ["hi", "bye"];
+// console.log(a);
+
+// **********;
+
+// BigInt exmple
+// let a= 999999999999n;
+// console.log(a);
+
+// **********;
+
+// undefined exmple
+// let a;
+// console.log(a);
+
+// **********;
+
+// Symbol exmple
+let a=Symbol('Hi');
+console.log(a);
