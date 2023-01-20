265781c80e17b10ca43732ec3d56f06f7f604686
ctwarit1 <ctwarit1@gmail.com>
2023-01-20 16:21:14 +0530

    Es9 feature js

---
+++ Es9.js
@@ -0,0 +1,45 @@
+// **********
+// 1)
+// Rest, Spread
+// function restParams(p1, p2, ...p3) {
+//     console.log(p1); // 1
+//     console.log(p2); // 2
+//     console.log(p3); // [3, 4, 5]
+// }
+// restParams(1, 2, 3, 4, 5);
+
+
+// **********
+// 2)
+// RegExp groups
+
+// const regExpDate = /([0-9]{4})-([0-9]{2})-([0-9]{2})/;
+// const match      = regExpDate.exec('2020-06-25');
+// const year       = match[1]; // 2020
+// const month      = match[2]; // 06
+// const day        = match[3]; // 25
+
+
+
+
+
+// **********
+// 3)
+// await in loop
+
+
+
+// **********
+// 4)
+//  promise.finally()
+
+
+// **********
+// 5)
+// Regexp dotAll
+
+
+
+// **********
+// 6)
+// Regexp lookahead Negative
