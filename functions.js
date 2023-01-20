143b2a28951761dd5fb6c94277b25c424ad2df52
ctwarit1 <ctwarit1@gmail.com>
2023-01-20 15:27:59 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,30 @@
+// Function declaration
+
+
+//    function add(a, b) {         
+//         console.log(a + b);
+//     }
+      
+//     // Calling a function
+//     add(2, 3);
+    
+// ***********
+
+// Anonymous Function
+
+
+    // var greet = function () {
+    //     console.log("Hello World!");
+    // };
+      
+    // greet();
+
+ // ***********
+
+// Arrow Function
+
+//  let add = (a, b) => a + b; {
+      
+//     console.log(add(3, 2));}
+ 
+// ***********
