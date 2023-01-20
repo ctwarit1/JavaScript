5a5d9dce7e4a25236524e41c47aea6e7351b1cc3
ctwarit1 <ctwarit1@gmail.com>
2023-01-20 16:18:55 +0530

    Es7 feature js

---
+++ Es7.js
@@ -0,0 +1,21 @@
+// ********
+// 1)
+// include
+// include function invented in ES7 update of javascript
+// and below is the description
+
+// const arr = [1, 2, 3, 4, 5];
+// // Check if there is the number 3 in the array
+// arr.include(3); // true
+
+// if (arr.include(3)) { ... }
+// // ... Equivalent to the previous writing of indexOf
+// arr.indexOf(3); // 2 (return its array position)
+// // If you want to write it in the if, you must add `> -1`, which is not as clear as the include in ES7 in terms of semantics
+// if (arr.indexOf(3) > -1) { ... }
+
+
+// ********
+// 2) Exponentiation Operator
+
+// console.log(Math.pow(2, 2)); 
